fd6598b2d927dec872c8c8bf03d498e3e6f06c6a
ramiel <ramielwan48@gmail.com>
2022-11-29 18:07:59 +0200

    feat: create example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,31 +1,52 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-reanimated-stretchy';
+import { StyleSheet, View, Text, SafeAreaView } from 'react-native';
+import { StretchyScrollView } from 'react-native-reanimated-stretchy';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
-
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
-
   return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StretchyScrollView
+        imageHeight={200}
+        imageSource={require('../assets/image.jpg')}
+        foreground={
+          <View style={styles.foreground}>
+            <Text style={styles.txt}>something here</Text>
+          </View>
+        }
+      >
+        {Array(20)
+          .fill(0)
+          .map((_, i) => (
+            <View style={styles.box}>
+              <Text>{i}</Text>
+            </View>
+          ))}
+      </StretchyScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  foreground: {
+    flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
   },
   box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
+    height: 100,
+    backgroundColor: 'pink',
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 10,
+    borderRadius: 10,
+  },
+  txt: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    color: 'orange',
   },
 });
